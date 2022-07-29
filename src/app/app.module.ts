677fffdf1c72ad4c491c645b0127c05d26dd8cec
Ervis Trupja <ervistrupja@outlook.com>
2022-07-29 22:14:46 +0200

    added test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-test',
+    templateUrl: './test.component.html',
+    styleUrls:['./test.component.css']
+})
+
+export class TestComponent {
+
+}
