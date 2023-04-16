23356faf7c6099741c191b1294b4d463abf1fba5
Ervis Trupja <ervistrupja@outlook.com>
2023-04-16 23:54:23 +0200

    Added Header and Footer of the App

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  currentYear: number = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit(): void {
