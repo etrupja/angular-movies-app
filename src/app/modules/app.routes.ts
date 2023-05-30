02a3579fd6f31f6c3a1b7e753388d8a3ca035efe
Ervis Trupja <ervistrupja@outlook.com>
2023-05-30 22:31:27 +0200

    Added Navigation to Application

--- src/app/modules/app.routes.ts
+++ src/app/modules/app.routes.ts
@@ -5,6 +5,7 @@ import { DetailsMoviesComponent } from '../components/details-movies/details-mov
 import { DetailsReviewsComponent } from '../components/details-reviews/details-reviews.component';
 import { DetailsComponent } from '../components/details/details.component';
 import { HomeComponent } from '../components/home/home.component';
+import { SearchComponent } from '../components/search/search.component';
 
 
 const routes: Routes = 
@@ -14,7 +15,9 @@ const routes: Routes =
         {path:'details', component: DetailsComponent},
         {path:'details/actors', component: DetailsActorsComponent},
         {path:'details/movies', component: DetailsMoviesComponent},
-        {path:'details/reviews', component: DetailsReviewsComponent}
+        {path:'details/reviews', component: DetailsReviewsComponent},
+        // {path:'feedback', component: FeedBackComponent}
+        {path:'search', component:SearchComponent}
       ];
 
 @NgModule({
