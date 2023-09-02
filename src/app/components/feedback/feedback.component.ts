fe0e27bdfff2db096775a4fe1447e9ba354e3361
Ervis Trupja <ervistrupja@outlook.com>
2023-09-03 00:30:31 +0200

    63. added reative forms validations

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Feedback } from 'src/app/data/model/Feedback';
-import {FormGroup, FormBuilder} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
@@ -12,16 +12,16 @@ export class FeedbackComponent implements OnInit {
   // feedback: Feedback = new Feedback('','',8,'','');
   feedbackForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.feedbackForm = this.fb.group({
-      emailAddress: [''],
-      phoneNumber: [''],
-      rate: [8],
-      feedbackTitle: [''],
-      feedback:['']
+    this.feedbackForm = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required, Validators.email]),
+      phoneNumber: new FormControl('', [Validators.required]),
+      rate: new FormControl(8, [Validators.min(0), Validators.max(10)]),
+      feedbackTitle: new FormControl('', [Validators.required]),
+      feedback:new FormControl('',[Validators.required])
     })
   }
 
