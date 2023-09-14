f339f1a23a326207eba44b7ab085be3518299d8b
Ervis Trupja <ervistrupja@outlook.com>
2023-09-14 23:27:29 +0200

    Add profile section pop-up

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,10 +9,17 @@ import { AuthService } from '@auth0/auth0-angular';
 export class NavbarComponent implements OnInit {
 
   public searchMovieTitle = ""
+  public loggedInUser: any;
 
   constructor(public _auth:AuthService) { }
 
   ngOnInit(): void {
+    if(this._auth.user$){
+      this._auth.user$.subscribe((data) => {
+        this.loggedInUser = data;
+        console.log('LoggedIn User = ', this.loggedInUser);
+      })
+    }
   }
 
 }
